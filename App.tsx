1d08b4989f12f602bc0163ce2c50ce759dc00ff9
peteygao <me@petergao.com>
2023-03-31 08:23:00 +0800

    bug: Convert Expo project to TS and import Video

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import { Video } from 'expo-av';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -6,6 +8,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Video resizeMode="cover" />
     </View>
   );
 }
